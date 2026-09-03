cdf4ba12370e5f83be8868c45f6112ae671b6afd
Fortune <fortunejite1234@gmail.com>
2026-09-03 16:12:01 +0100

    feat: add WelcomeScreen component with typewriter effect and animations
    - Introduced a new WelcomeScreen component that displays a loading animation with typewriter text effect.
    - Integrated framer-motion for smooth transitions and animations.
    - Added AOS (Animate On Scroll) for additional visual effects.
    - Updated package.json to include new dependencies: aos and framer-motion.
    - Modified the main page to include the WelcomeScreen and handle its loading state.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
+'use client';
+
+import WelcomeScreen from '@/components/WelcomeScreen';
+import { AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
+
 export default function Home() {
+  const [showWelcome, setShowWelcome] = useState(true);
   return (
     <div>
+      <AnimatePresence mode="wait">
+        {showWelcome && (
+          <WelcomeScreen onLoadingComplete={() => setShowWelcome(false)} />
+        )}
+      </AnimatePresence>
+
+      <>
+        <footer>
+          <center>
+            <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+            <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+              © {new Date().getFullYear()}{' '}
+                Fortune™
+              . All Rights Reserved.
+            </span>
+          </center>
+        </footer>
+      </>
     </div>
   );
 }
