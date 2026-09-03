0f396542e03458590349a6054d3f976a6cdf07df
Fortune <fortunejite1234@gmail.com>
2026-09-03 17:01:50 +0100

    feat: refactor font usage and add Home component with typing effect and animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,37 @@
 'use client';
 
 import AnimatedBackground from '@/components/Background';
+import Home from '@/components/Home';
 import Navbar from '@/components/Navbar';
 import WelcomeScreen from '@/components/WelcomeScreen';
 import { AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
 
-export default function Home() {
+export default function HomePage() {
   const [showWelcome, setShowWelcome] = useState(true);
   return (
-    <div>
+    <>
       <AnimatePresence mode="wait">
         {showWelcome && (
           <WelcomeScreen onLoadingComplete={() => setShowWelcome(false)} />
         )}
       </AnimatePresence>
 
-      <>
-        <Navbar />
-        <AnimatedBackground />
-        <footer>
-          <center>
-            <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
-            <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-              © {new Date().getFullYear()}{' '}
-                Fortune™
-              . All Rights Reserved.
-            </span>
-          </center>
-        </footer>
-      </>
-    </div>
+      {!showWelcome && (
+        <>
+          <Navbar />
+          <AnimatedBackground />
+          <Home />
+          <footer>
+            <center>
+              <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+              <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+                © {new Date().getFullYear()} Fortune™ . All Rights Reserved.
+              </span>
+            </center>
+          </footer>
+        </>
+      )}
+    </>
   );
 }
